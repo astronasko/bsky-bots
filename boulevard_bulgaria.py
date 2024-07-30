a9e584afd208e834cf8338a40503df2409e8ea47
Atanas Stefanov <astronasko@gmail.com>
2024-07-30 14:10:06 +0100

    Initial commit

---
+++ boulevard_bulgaria.py
@@ -0,0 +1,48 @@
+import atproto
+import feedparser
+import json
+import os
+import urllib.request
+
+USER = "boulevardbulgaria.bsky.social"
+PASS = os.environ["BB_PASS"]
+BSKY_CLIENT = atproto.Client().login(USER, PASS)
+CACHE_FILENAME = "cache/boulevard_bulgaria.json"
+
+# Load cache
+with open(CACHE_FILENAME, "r") as file:
+    cache = json.load(file)
+    etag = cache["etag"]
+    entry_ids = cache["entry_ids"]
+
+feed_dict = feedparser.parse(
+    "https://boulevardbulgaria.bg/feed.atom",
+    etag=etag,
+)
+cache["etag"] = feed_dict.etag
+
+for entry in feed_dict.entries:
+    if entry.id not in entry_ids:
+        entry_link = entry.link
+        entry_thumb = entry.media_content[0]["url"]
+        entry_title = entry.title
+        while len(entry_title)>150:
+            entry_title = " ".join(entry_title.split(" ")[:-1])
+            entry_title += ".."
+
+        text_builder = atproto.client_utils.TextBuilder()
+        text_builder.text(f"{entry_title}. ")
+        text_builder.link("линк", entry_link)
+        BSKY_CLIENT.send_post(
+            text=text_builder,
+            image=urllib.request.urlopen(entry_thumb).read(),
+            langs=["bg"]
+        )
+        cache["entry_ids"].append(entry.id)
+
+cache_len = len(cache["entry_ids"])
+if cache_len > 1000:
+    cache["entry_ids"] = cache["entry_ids"][cache_len-1000:]
+
+with open(CACHE_FILENAME, "w") as file:
+    json.dump(cache, file, indent=4)
